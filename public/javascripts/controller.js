05a376826c8621a8b7259147a55ed0de60b4f6aa
sulianggang <sulianggang@askingdata.com>
2016-11-21 22:01:30 +0800

    路由创建

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,4 +1,62 @@
 angular.module('ctrlModule', [])
 	.controller('testCtrl', function ($scope, $http) {
-	    $scope.test = '测试'
+	    $scope.menus =  [
+					      {
+					        "name": "行业统计",
+					        "submenus": null,
+					        "classes": "fa-bar-chart-o",
+					        "state": "test.view1"
+					      },
+					      {
+					        "name": "商品榜",
+					        "submenus": [{"name": "热销榜", "state": "test.view2({type:\"0\",menuName:\"热销榜\"})"}, {
+					          "name": "飙升榜",
+					          "state": "test.view3({type:\"1\",menuName:\"飙升榜\"})"
+					        }],
+					        "classes": "fa-bar-chart-o",
+					        "state": "test.view1"
+					      }, {
+					        "name": "店铺榜",
+					        "submenus": [{"name": "热销榜", "state": "test.view4({type:\"0\",menuName:\"热销榜\"})"}, {
+					          "name": "飙升榜",
+					          "state": "test.view5({type:\"1\",menuName:\"飙升榜\"})"
+					        }],
+					        "classes": "fa-area-chart",
+					        "state": "test.view1"
+					      }, {
+					        "name": "标签榜",
+					        "submenus": [{"name": "热销榜", "state": "test.view1({type:\"0\",menuName:\"热销榜\"})"}, {
+					          "name": "飙升榜",
+					          "state": "test.view2({type:\"0\",menuName:\"飙升榜\"})"
+					        }],
+					        "classes": "fa-indent",
+					        "state": "test.view1"
+					      }, {
+					        "name": "我的关注",
+					        "submenus": [{"name": "商品", "state": "test.view3"}, {
+					          "name": "店铺",
+					          "state": "test.view4"
+					        }, {"name": "标签", "state": "test.view5"}],
+					        "classes": "fa-tasks",
+					        "state": "test.view1"
+					      },
+					      {
+					        "name": "搜索",
+					        "submenus": null,
+					        "classes": "fa-search",
+					        "state": "test.view1"
+					      }
+					    ]
+
+
+			// console.log(nowstate())
+
+
+
+
+
+	})
+	.controller('test1Ctrl', function ($scope, $http) {
+	    $scope.test="ss"
+
 	})
--- public/javascripts/routers.js
+++ public/javascripts/routers.js
@@ -7,8 +7,10 @@ angular.module('myApp', ['ui.router','ctrlModule'])
       });
 
       $urlRouterProvider
-      .when('/','/test')
-      .otherwise('/test')
+      .when('/','/test/view1')
+      .otherwise('/test/view1')
+
+
 
       $stateProvider
       .state('test',{
@@ -16,5 +18,61 @@ angular.module('myApp', ['ui.router','ctrlModule'])
         templateUrl:'views/test.html',
         controller:'testCtrl'
       })
+
+
+
+
+
+      .state('test.view1',{
+        url:'/view1',
+        views:{
+          "content":{
+            templateUrl:'views/view1.html'
+          }
+        }
+      })
+      .state('test.view2',{
+        url:'/view2',
+        views:{
+          "content":{
+            templateUrl:'views/view2.html'
+          }
+        }
+      })
+      .state('test.view3',{
+        url:'/view3',
+        views:{
+          "content":{
+            templateUrl:'views/view3.html'
+          }
+        }
+      })
+      .state('test.view4',{
+        url:'/view4',
+        views:{
+          "content":{
+            templateUrl:'views/view4.html'
+          }
+        }
+      })
+      .state('test.view5',{
+        url:'/view5',
+        views:{
+          "content":{
+            templateUrl:'views/view5.html'
+          }
+        }
+      })
+    })
+    // .run(function ($rootScope) {
+    //    $rootScope.$on('$stateChangeSuccess', function (event,toState,toParam,fromState,fromParams) {
+    //         console.log(toState.name)
+    //     })
+    // });
+
+    .factory('nowstate', function ($rootScope) {
+      $rootScope.$on('$stateChangeSuccess',function(event, toState, toParams, fromState, fromParams){
+        return toState.name
+      });
     })
  
